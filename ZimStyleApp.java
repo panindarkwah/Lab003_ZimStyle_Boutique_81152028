ab59e2f38b2c0447da14f30b3f7c841454910560
panindarkwah <panindarkwah@gmail.com>
2025-11-19 10:20:52 +0000

    Add files via upload

---
+++ ZimStyleApp.java
@@ -0,0 +1,20 @@
+import  java.util.Scanner;
+public class ZimStyleApp {
+    public static void main(String[] args) {
+        String name = "dress".toUpperCase();
+        ClothingType type = ClothingType.SHIRT;
+        Scanner sc = new Scanner(System.in);
+
+
+        //switch (type) {
+            //ClothingType userInput = ClothingType.valueOf(name);
+            //System.out.print(userInput);
+    // Instance of the clothing item
+    //ClothingItem item = new ClothingItem();
+    //item
+
+
+
+        }
+    }
+}
