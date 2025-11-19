cf1dd26a4c557dfd154f5a911916b552f30c8245
panindarkwah <panindarkwah@gmail.com>
2025-11-19 10:19:22 +0000

    Add files via upload

---
+++ ClothingItem.java
@@ -0,0 +1,26 @@
+public class ClothingItem {
+    private String name;
+    private double price;
+    private Size size;
+    private ClothingType type;
+
+    private class int itemCount()
+}
+public String getName(){
+    return name;
+}
+
+
+public class ClothingItem {
+    this.name=name;
+}
+public double getPrice() {
+    return price;
+}
+public void getPrice(double price){
+    this.price = price;
+}
+public
+
+
+// Alt insert    to get all the setup
